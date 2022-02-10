63d9f4bc959f5070efeaa608e4a89db1681445d1
Ulises Gomez <ulisesgomez120@gmail.com>
2022-02-10 17:40:34 -0600

    added react and finished webpack config

--- addingReact-sec5/webpack.config.js
+++ addingReact-sec5/webpack.config.js
@@ -1,16 +1,35 @@
 const path = require("path");
-
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
-  entry: "./src/test.tsx",
+  devtool: "cheap-module-source-map",
+  entry: {
+    popup: path.resolve("src/popup/popup.tsx"),
+  },
   module: {
     rules: [{ use: "ts-loader", test: /\.tsx?$/, exclude: /node_modules/ }],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve("src/manifest.json"),
+          to: path.resolve("dist"),
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      title: "React Extension",
+      filename: "popup.html",
+      chunks: ["popup"],
+    }),
+  ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    path: path.resolve("dist"),
   },
 };
