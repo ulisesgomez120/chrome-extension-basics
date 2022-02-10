e52bae617730d8ff4df227bbec1db20dfd9e87ce
Ulises Gomez <ulisesgomez120@gmail.com>
2022-02-10 14:58:14 -0600

    commented out code to test function

--- test1-navToNewPage/popup/popup.js
+++ test1-navToNewPage/popup/popup.js
@@ -1,39 +1,40 @@
-chrome.storage.local.get(["ticker", "tickerData"], (res) => {
-  let { ticker, tickerData } = res;
-  if (ticker && ticker === tickerData.ticker) {
-    createTempDom(ticker, tickerData);
-  } else if (ticker) {
-    fetch(`https://testext.netlify.app/api/getSite?ticker=${ticker}`, {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        createTempDom(ticker, data);
-        chrome.storage.local.set({ tickerData: data });
-      })
-      .catch((err) => console.log(err));
-  } else {
-    return;
-  }
-});
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.ticker !== "") {
-    const ticker = message.ticker;
-    fetch(`https://testext.netlify.app/api/getSite?ticker=${ticker}`, {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        createTempDom(ticker, data);
-        chrome.storage.local.set({ tickerData: data });
-      })
-      .catch((err) => console.log(err));
-  }
-});
+// chrome.storage.local.get(["ticker", "tickerData"], (res) => {
+//   let { ticker, tickerData } = res;
+//   if (ticker && ticker === tickerData.ticker) {
+//     createTempDom(ticker, tickerData);
+//   } else if (ticker) {
+//     fetch(`https://testext.netlify.app/api/getSite?ticker=${ticker}`, {
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+//       },
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         createTempDom(ticker, data);
+//         chrome.storage.local.set({ tickerData: data });
+//       })
+//       .catch((err) => console.log(err));
+//   } else {
+//     return;
+//   }
+// });
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.ticker !== "") {
+//     const ticker = message.ticker;
+//     fetch(`https://testext.netlify.app/api/getSite?ticker=${ticker}`, {
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+//       },
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         createTempDom(ticker, data);
+//         chrome.storage.local.set({ tickerData: data });
+//       })
+//       .catch((err) => console.log(err));
+//   }
+// });
+createTempDom("HTN", { price: "1000" });
 function createTempDom(ticker, tickerData) {
   document.body.innerText = "";
   const header = document.createElement("h1");
